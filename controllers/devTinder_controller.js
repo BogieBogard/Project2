865c2d3dbebd5201512a23544e4ecc7ed59eb6c6
TheMelonOne <paichun.wang@yahoo.com>
2018-09-18 18:05:14 -0500

    saving changes, still working on dev profile

--- controllers/devTinder_controller.js
+++ controllers/devTinder_controller.js
@@ -7,8 +7,5 @@ router.get("/", (req, res) => {
   console.log("Default Page /");
   res.render("index");
 });
-router.get("/#", (req, res) => {
-  console.log("LInk");
-});
 
 module.exports = router; // why does this export custom methods too?
