2c72df123fa397a2b8b7e78e370b4372f4479dfd
TheMelonOne <paichun.wang@yahoo.com>
2018-09-18 14:57:25 -0500

    added dev profile page, with infinite scrolling effect

--- controllers/devTinder_controller.js
+++ controllers/devTinder_controller.js
@@ -7,5 +7,8 @@ router.get("/", (req, res) => {
   console.log("Default Page /");
   res.render("index");
 });
+router.get("/#", (req, res) => {
+  console.log("LInk");
+});
 
 module.exports = router; // why does this export custom methods too?
