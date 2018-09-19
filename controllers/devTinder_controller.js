789f91a8c64bc90a369f121baddf1f076c7a0637
TheMelonOne <paichun.wang@yahoo.com>
2018-09-19 15:08:26 -0500

    saving route progress

--- controllers/devTinder_controller.js
+++ controllers/devTinder_controller.js
@@ -1,11 +1,46 @@
 let express = require("express");
 
 let router = express.Router();
-
-
+// MAIN TESTING PAGE: CAN BE DELETED AT ANYTIME AFTER DEPLOYMENT
+router.get("/allpagedisplay", (req, res) => {
+  console.log("Front End All Page Test Call");
+  res.render("layouts/allpageload");
+});
+// Main/Index Page
 router.get("/", (req, res) => {
-  console.log("Default Page /");
+  console.log("Index Main Page");
   res.render("index");
 });
+// developer login -- forget password -- Signup
+router.get("/developerlogin", (req, res) => {
+  console.log("Developer Main Page");
+  res.render("partials/Developer/developerlogin")
+});
+// develoer login > Signup
+router.get("/newdeveloper", (req, res) => {
+  console.log("Developer Signup");
+  res.render("partials/Developer/developerSignup")
+});
 
+// customer login -- forget password -- Signup
+router.get("/customerlogin", (req, res) => {
+  console.log("Customer Main Page");
+  res.render("partials/Customer/customerlogin")
+});
+// customer login > Signup
+router.get("/newcustomer", (req, res) => {
+  console.log("Customer Signup");
+  res.render("partials/Customer/customerSignup")
+});
+
+
+router.get("/123", (req, res) => {
+  console.log("Developer Profile");
+  res.render("partials/Developer/developerProfile")
+});
+
+router.get("/456", (req, res) => {
+  console.log("Customer Profile");
+  res.render("partials/Customer/customerSignup")
+});
 module.exports = router; // why does this export custom methods too?
---
+++ public/assets/js/devTinder.js
@@ -0,0 +1,15 @@
+$(() => {
+    console.log("devTinder.js Loaded")
+    $(".change-sleep").on("click", function(event) {
+        $.ajax("/" + id, {
+          type: "GET",
+          data: newSleepState
+        }).then(
+          function() {
+            console.log("changed sleep to", newSleep);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+});
